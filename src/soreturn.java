1f10266ce1125d487fa1e8cb9022158063b1e25d
david.elim45 <davidelim170204@gmail.com>
2024-01-23 18:03:12 +0100

    MyFunc

---
+++ src/soreturn.java
@@ -0,0 +1,13 @@
+
+public class soreturn {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=myFunc(x);
+		System.out.println(y);
+
+	}
+public static int myFunc(int x) {
+	return x*3;
+}
+}
